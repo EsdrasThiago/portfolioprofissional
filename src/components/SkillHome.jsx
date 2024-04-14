c40b7ff32fb0f8451322521ce30bfda81b194e42
EsdrasThiago <esdras12thiago@gmail.com>
2024-04-14 12:32:35 -0300

    Habilidades na Home estilizada

--- src/components/SkillHome.jsx
+++ src/components/SkillHome.jsx
@@ -1,18 +1,27 @@
 import skillsHomeMock from "../mocks/skillsMock";
+import '../styles/SkillsHomeStyle.css'
 
 function SkillHome() {
 
     return (
-        <section>
-            <h1><b>#</b>Habilidades</h1>
-            {skillsHomeMock.map((e) => (
-                <section>
-                    <h1>{e.name}</h1>
-                    {e.text.map((el) => (
-                        <p>{el}</p>
-                    ))}
-                </section>
-            ))}
+        <section className="skills-home-main">
+            <section className="skill-home-title">
+                <h1><b>#</b>Habilidades</h1>
+                <hr />
+            </section>
+            <section className="skills-home-cards">
+                {skillsHomeMock.map((e) => (
+                    <section className="skills-home-card">
+                        <h1 className="skills-home-title">{e.name}</h1>
+                        <hr />
+                        <section className="skills-home-text">
+                            {e.text.map((el) => (
+                                <p>{el}</p>
+                            ))}
+                        </section>
+                    </section>
+                ))}
+            </section>
         </section>
     );
 }
--- src/mocks/skillsMock.js
+++ src/mocks/skillsMock.js
@@ -4,25 +4,25 @@ const skillsHomeMock = [
         text: ["TypeScript", "Python", "JavaScript", "Java"]
     },
     {
-        name: "Ferramentas",
-        text: ["Linux", "Git", "VSCode", "Figma", "Insomnia", "Eclipse", "Excel", "PowerBI"]
-    },
-    {
         name: "Frameworks",
         text: ["React", "Express.js", "Bootstrap"]
     },
     {
-        name: "Banco de Dados",
-        text: ["MongoDB", "NoSQL", "MySQL"]
+        name: "Testes",
+        text: ["Jest", "RTL", "Sinon", "Chai", "Mocha"]
+    },
+    {
+        name: "Ferramentas",
+        text: ["Linux", "Git", "VSCode", "Figma", "Insomnia", "Eclipse", "Excel", "PowerBI"]
     },
     {
         name: "Outros",
         text: ["HTML", "CSS", "Sequelize", "REST", "Excel", "PowerBI"]
     },
     {
-        name: "Testes",
-        text: ["Jest", "RTL", "Sinon", "Chai", "Mocha"]
-    }
+        name: "Banco de Dados",
+        text: ["MongoDB", "NoSQL", "MySQL"]
+    },
 ]
 
 export default skillsHomeMock;
