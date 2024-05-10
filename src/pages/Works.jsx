662f6299f46dedf6760f9afb3ebeaa037a1c40cd
EsdrasThiago <esdras12thiago@gmail.com>
2024-05-10 20:48:30 -0300

    Arrumando espaçamentos dos projetos

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,30 +1,32 @@
 import Header from "../components/Header";
 import projectsHomeMock from "../mocks/projectsMock";
+import "../styles/Works.css"
 
 function Works() {
 
     return (
-        <section>
+        <section className="project-main">
             <Header page="projects" />
-            <section>
+            <section className="project-title-main">
                 <h1><b>/</b>Projetos</h1>
-                <p>Uma lista dos meus projetos</p>
+                <p>Lista dos meus projetos</p>
             </section>
-            <section>
-                <h1><b>#</b>projetos-completos</h1>
+            <section className="complete-projects-main">
+                <h1 className="complete-projects-main-title"><b>#</b>projetos-completos</h1>
                 <section className="project-home-cards">
-                {projectsHomeMock.map((e) => (
-                    <section className="project-home-card">
-                        <img src={e.image} alt={e.name} className="project-home-img"/>
-                        <p className="project-home-stacks">{e.stacks}</p>
-                        <hr className="project-home-hr-bot" />
-                        <h1 className="project-home-title-card">{e.name}</h1>
-                        <p className="project-home-description">{e.description}</p>
-                        <button>Site</button>
-                    </section>
-                ))}
+                    {projectsHomeMock.map((e) => (
+                        <section className="project-home-card">
+                            <img src={e.image} alt={e.name} className="project-home-img" />
+                            <p className="project-home-stacks">{e.stacks}</p>
+                            <hr className="project-home-hr-bot" />
+                            <h1 className="project-home-title-card">{e.name}</h1>
+                            <p className="project-home-description">{e.description}</p>
+                            <button>Site</button>
+                        </section>
+                    ))}
                 </section>
             </section>
+                    <section className="works-space" />
         </section>
     );
 }
